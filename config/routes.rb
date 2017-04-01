d033f95f7927873b38ab5bb898fb97816bcdbafc
Eric Mwenda <ericmwenda5@gmail.com>
2017-04-01 14:23:48 +0300

    adding auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-resources :articles do
-  resources :comments
-end
+  resources :articles do
+    resources :comments
+  end
   resources :tags
+  resources :author_sessions, only: [ :new, :create, :destroy ]
+  get 'login'  => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
 root to: 'articles#index'
+
 end
